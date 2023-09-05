0f7ae12bda6d895fdfa8c6d4b91360824cad252f
MrTreev <mr.treev@gmail.com>
2023-09-05 13:42:20 +1000

    better treesitter for obsidian

--- lua/mrtreev/init.lua
+++ lua/mrtreev/init.lua
@@ -9,7 +9,7 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-	require("plenary.reload").reload_module(name)
+    require("plenary.reload").reload_module(name)
 end
 
 vim.g.netrw_browse_split = 0
