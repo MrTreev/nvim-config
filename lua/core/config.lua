f762bc5f2e9430ee4ec8366bb17c4bcd41af432d
MrTreev <mr.treev@gmail.com>
2024-06-14 09:05:47 +1000

    sync

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -1,7 +1,7 @@
 vim.opt.clipboard:append { "unnamedplus" }
 
 vim.opt.colorcolumn = "0"
-vim.opt.guicursor = ""
+vim.opt.textwidth = 0
 vim.opt.hidden = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -60,20 +60,18 @@ vim.api.nvim_create_autocmd("BufEnter", {
         local bufnr = vim.api.nvim_get_current_buf()
         local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":p")
         local command = "editorconfig " .. vim.fn.fnameescape(filename)
-        local default_line_length = 80
-        local max_line_length = nil
+        local default_line_length = "80"
+        local set_line_length = nil
         for _, line in ipairs(vim.fn.systemlist(command)) do
             local found_length = line:match("max_line_length%s*=%s*(%d+)")
             if found_length then
-                max_line_length = found_length
+                set_line_length = found_length
                 break
             end
         end
-        if max_line_length then
-            vim.api.nvim_set_option_value('colorcolumn', max_line_length, { win = 0 })
-            vim.api.nvim_set_option_value('textwidth', tonumber(max_line_length), { buf = bufnr })
-        else
-            vim.api.nvim_set_option_value('colorcolumn', default_line_length, { win = 0 })
+        if set_line_length == nil then
+            set_line_length = default_line_length
         end
+        vim.api.nvim_set_option_value('colorcolumn', set_line_length, { win = 0 })
     end,
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,12 +6,13 @@ return {
     },
     build = ":TSUpdate",
     config = function()
+        vim.treesitter.language.register('hyprlang', '.conf')
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+            ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "hyprlang" },
             sync_install = false,
             auto_install = true,
             highlight = {
-                enable = true,
+                enable = false,
                 additional_vim_regex_highlighting = { "markdown" },
             },
             indent = { enable = true },
